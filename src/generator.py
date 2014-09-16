2beb31474d87f5df1863b647a7d483669d2aade9
jshoham <jshoham@uci.edu>
2014-09-16 04:20:18 -0700

    grid and generator partially working

--- src/generator.py
+++ src/generator.py
@@ -1,22 +1,87 @@
-#generator
+# generator
 
-import grid
+from grid import Grid
+from random import *
 
 inputFile = 'gen_input.txt'
 outputFile = 'gen_output.txt'
 
 
 def readfile(inputFile):
-	f = open(inputFile, 'r')
-	line = f.readline().split()
-	if len(line) == 4:
-		N, p, q, M = line
-	else:
-		print "input error"
-		
-#generate a puzzle
+    f = open(inputFile, 'r')
+    line = f.readline().split()
+    if len(line) == 4:
+        N, p, q, M = line
+    else:
+        print "input error"
 
+
+def verifyInput(N, p, q, M):
+    assert(N > 0)
+    assert(p > 0)
+    assert(q > 0)
+    assert(M > 0)
+    assert(p*q == N)
+    assert(M <= N**N)
+
+
+
+# bug: if a cell's possible values is reduced to 1 by constraint propagation then the cell
+# is considered solved but it is not reflected in the 'filled' count. Thus when the board
+# becomes full it gets stuck in an infinite loop looking for nonexistent vacant cells
 def generate(N, p, q, M):
-	for x in range(M):
-		assign(grid)
+    filled = 0
+    board = Grid(N, p, q, M)
+    while filled < M:
+        i = randint(0, N - 1)
+        j = randint(0, N - 1)
+        possible_values = board.grid[i][j]
+        if len(possible_values) == 0:
+            board.clear()
+            filled = 0
+            print 'restarting'
+        elif len(possible_values) > 1:
+            if board.choose(i, j, sample(possible_values, 1)[0]):
+                filled += 1
+                print 'new', filled
+                board.display()
 
+    return board
+
+
+# Generate a random puzzle by repeatedly picking a random vacant cell and assigning a
+# random token to it. If the token violates any of the constraints then undo that
+# assignment, delete that token from the list of possible tokens at that cell, and
+# repeat; if the list of possible tokens at that cell becomes empty, then fail globally
+# (do not backtrack). Repeat until M cells have been filled. If you failed globally
+# before the random puzzle was generated successfully, then execute a random restart and
+# try again. Keep trying until success.
+def generate2(N, p, q, M):
+    filled = 0
+    board = Grid(N, p, q, M)
+    while filled < M:
+        i = randint(0, N - 1)
+        j = randint(0, N - 1)
+
+        while not board.cellFilled(i, j):
+            possible_values = board.grid[i][j]
+            if len(possible_values) == 0:
+                board.clear()
+                filled = 0
+                print 'restarting'
+                break
+            else:
+                random_value = sample(possible_values, 1)[0]
+                if board.violatesConstraints(i, j, random_value):
+                    possible_values.discard(random_value)
+                else:
+                    board.assign(i, j, random_value)
+                    filled += 1
+                    print 'chose', filled, 'values', 'added', random_value, 'to', i, j
+                    board.display()
+    return board
+
+
+#print str(generate(9, 3, 3, 25))
+
+generate2(2, 2, 1, 4).display()
--- src/grid.py
+++ src/grid.py
@@ -1,40 +1,135 @@
 # grid
 from random import randint
 
+
 class Grid(object):
-	
-	def __init__(self, N=9, p=3, q=3):
-		self.N = N		# The number of tokens
-		self.p = p		# The number of rows per block
-		self.q = q		# The number of columns per block
-		self.grid = [[[x for x in xrange(N)] for i in xrange(N)] for j in xrange(N)]
-		
-	def display(self):
-		width = len(str(self.N))+1
-		rowSeperator = '+'.join(['-'*(width*self.q)]*self.p)
-		
-		for row in range(self.N):
-			if row%self.p == 0 and row != 0: print rowSeperator
-			
-			line = ''
-			for cell in range(self.N):
-				if cell%self.q == 0 and cell != 0: line += '|'
-				
-				cellValues = self.grid[row][cell]
-				if len(cellValues) == 1 and cellValues[0] != 0:
-					line += str(cellValues[0]).center(width)
-				else:
-					line += '.'.center(width)
-				
-			print line
-	
-	def randomFill(self):
-		for i in range(self.N):
-			for j in range(self.N):
-				randomValue = randint(0, self.N)
-				self.grid[i][j]= [randomValue]
-				
-	def clear(self):
-		for i in range(self.N):
-			for j in range(self.N):
-				del self.grid[i][j][:]
+    def __init__(self, N=9, p=3, q=3, M=0):
+        self.N = N  # The number of tokens
+        self.p = p  # The number of rows per block
+        self.q = q  # The number of columns per block
+        self.M = M  # The number of tokens initially given
+        self.grid = [[Cell(N) for i in xrange(N)] for j in xrange(N)]
+
+    def __str__(self):
+        grid_str = str(self.N) + " " + str(self.p) + " " + str(self.q) + " " \
+                   + str(self.M) + "\n"
+
+        for row in range(self.N):
+            for col in range(self.N):
+                grid_str += str(self.cellValue(row, col)) + " "
+            grid_str += '\n'
+        return grid_str
+
+    def display(self, x=None, y=None):
+        width = len(str(self.N)) + 1
+        row_separator = '+'.join(['-' * (width * self.q)] * self.p)
+
+        for row in range(self.N):
+            if row%self.p == 0 and row != 0: print row_separator
+            line = ''
+            for col in range(self.N):
+                if col%self.q == 0 and col != 0: line += '|' # column separator
+                cell_value = self.cellValue(row, col)
+                if row == x and col == y:
+                    cell_value = '*'
+                if cell_value == 0:
+                    line += '.'.center(width)
+                else:
+                    line += str(cell_value).center(width)
+            print line
+
+    def displayCell(self, x, y):
+        print 'Contents of cell at (', x, ',', y, '):', self.grid[x][y]
+
+    def randomFill(self):
+        for i in range(self.N):
+            for j in range(self.N):
+                cell = self.grid[i][j]
+                cell.clear()
+                cell.add(randint(0, self.N))
+
+    def clear(self):
+        for i in range(self.N):
+            for j in range(self.N):
+                cell = self.grid[i][j]
+                cell.clear()
+                cell.update(range(1,self.N+1))
+
+    # sets the cell at (x, y) to value, then runs a constraint propagation
+    def choose(self, x, y, value):
+        possible_values = self.grid[x][y]
+        if value in possible_values:
+            self.propagateConstraints(x, y, value)
+            possible_values.clear()
+            possible_values.add(value)
+            return True
+        else:
+            return False
+
+    # sets the cell at (x, y) to value, without running a constraint propagation
+    def assign(self, x, y, value):
+        possible_values = self.grid[x][y]
+        if value in possible_values:
+            possible_values.clear()
+            possible_values.add(value)
+            return True
+        else:
+            return False
+
+    def propagateConstraints(self, x, y, value):
+        # remove value as a candidate from all peers in the same box
+        upperleft_x = x - x%self.p
+        upperleft_y = y - y%self.q
+        for row in range(upperleft_x, upperleft_x+self.p):
+            for col in range(upperleft_y, upperleft_y+self.q):
+                self.grid[row][col].discard(value)
+
+        # remove value as a candidate from all peers in the same row and column
+        for cell in range(self.N):
+            self.grid[x][cell].discard(value)
+            self.grid[cell][y].discard(value)
+
+        # the cell at (x, y) will be cleared during the propagation process so re-add it
+        self.grid[x][y].add(value)
+
+
+    # checks if placing value in the cell at (x, y) will violate a row/column/box constraint
+    def violatesConstraints(self, x, y, value):
+        if value == 0: # zero designates an empty cell and thus never causes a violation
+            return False
+
+        # first check if value is contained in a peer cell in the same box
+        upperleft_x = x - x%self.p
+        upperleft_y = y - y%self.q
+        for row in range(upperleft_x, upperleft_x+self.p):
+            for col in range(upperleft_y, upperleft_y+self.q):
+                if self.cellValue(row, col) == value:
+                   return True
+
+        # next check if value is contained in a peer cell in the same row or column
+        for cell in range(self.N):
+            if self.cellValue(cell, y) == value:
+                return True
+            if self.cellValue(x, cell) == value:
+                return True
+
+        return False
+
+
+    def cellFilled(self, x, y):
+        cell = self.grid[x][y]
+        return len(cell) == 1
+
+    def cellValue(self, x, y):
+        if self.cellFilled(x, y):
+            (cell_value,) = self.grid[x][y]
+            return cell_value
+        else:
+            return 0
+
+class Cell(object):
+
+    def __init__(self, N):
+        self.token = None
+        self.possible_tokens = set(range(1, N+1))
+
